c991eb974765597834bc1b7c39719b2e207a1c20
lunasirena <110263613+lunasirena@users.noreply.github.com>
2022-08-02 15:20:37 +0200

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,59 @@
+export const sounds = [
+  [ // row 1
+    {
+      filename: 'Sound 1.1.wav',
+      level: 3,
+    }, {
+      filename: 'Sound 1.2.wav',
+      level: -7,
+    }, {
+      filename: 'Sound 1.3.wav',
+      level: 3
+    }, {
+      filename: 'Sound 1.4.wav',
+      level: 10,
+    }
+  ], [ // row 2
+    {
+      filename: 'Sound 2.1.wav',
+      level: 7,
+    }, {
+      filename: 'Sound 2.2.wav',
+      level: 3,
+    }, {
+      filename: 'Sound 2.3.wav',
+      level: 8,
+    }, {
+      filename: 'Sound 2.4.wav',
+      level: 3,
+    }
+  ], [ // row 3
+    {
+      filename: 'Sound 3.1.wav',
+      level: 3,
+    }, {
+      filename: 'Sound 3.2.wav',
+      level: 3,
+    }, {
+      filename: 'Sound 3.3.wav',
+      level: 7,
+    }, {
+      filename: 'Sound 3.4.wav',
+      level: 5,
+    }
+  ], [ // row 4
+    {
+      filename: 'Sound 4.1.wav',
+      level: 4,
+    }, {
+      filename: 'Sound 4.2.wav',
+      level: 13,
+    }, {
+      filename: 'Sound 4.3.wav',
+      level: -22,
+    }, {
+      filename: 'Sound 4.4.wav',
+      level: -25,
+    }
+  ]
+];
